755f290aa0f18b2cea3c09afc940085bc5d2119c
Robert Lewis <rswl86@gmail.com>
2022-07-19 20:18:17 -0400

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{ name }}</div>
+</template>
+
+<script setup lang="ts">
+const name = ref("");
+onMounted(async () => {
+  const res = await fetch("/api/hello/Rob");
+  const json = await res.json();
+  name.value = json.name;
+});
+</script>
+
+<style scoped></style>
